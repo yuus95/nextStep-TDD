a6fbd45991ab02e66ba90e95094ccd41a792ab16
yushin <kkad45@naver.com>
2022-02-20 15:12:15 +0900

    입력우선계산기 리팩토링 이전 코드 추가

--- src/test/kotlin/string/InputPriority.kt
+++ src/test/kotlin/string/InputPriority.kt
@@ -71,4 +71,44 @@ class InputPriority {
         val result = inputPriority.buildInputPriorityCalculator(data)
         println(result)
     }
+
+
+    @Test
+    fun `리팩토링이전코드`() {
+        //given
+        val data = "2 + 10 * 4 / 2"
+        val numberList = mutableListOf<Int>()
+        val arithmeticOperation = mutableListOf<String>()
+        val stdin = System.`in`
+        System.setIn(ByteArrayInputStream(data.toByteArray()))
+        val scanner = Scanner(System.`in`)
+        System.setIn(stdin)
+        val stringList = scanner.nextLine().split(" ")
+        for (str in stringList) {
+            try {
+                numberList.add(str.toInt())
+            } catch (e: NumberFormatException) {
+                arithmeticOperation.add(str)
+            }
+        }
+
+        var sum = numberList[InputPriorityCalculator.START_INDEX]
+        for (i in 1..numberList.lastIndex) {
+            when (arithmeticOperation[i - 1]) {
+                "+" -> {
+                    sum += numberList[i]
+                }
+                "-" -> {
+                    sum -= numberList[i]
+                }
+                "/" -> {
+                    sum /= numberList[i]
+                }
+                "*" -> {
+                    sum *= numberList[i]
+                }
+            }
+        }
+        println(sum)
+    }
 }
