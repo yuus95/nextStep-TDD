bc2ec388da6969c1e0682ed1f940ea1449f8b6b9
yushin <kkad45@naver.com>
2022-02-20 15:07:01 +0900

    입력우선계산기 개발 완료

--- src/test/kotlin/string/InputPriority.kt
+++ src/test/kotlin/string/InputPriority.kt
@@ -8,19 +8,67 @@ import kotlin.String
 
 class InputPriority {
 
+    class InputPriorityCalculator {
+        private var data: List<String> = listOf()
+        private val numberList = mutableListOf<Int>()
+        private val arithmeticOperation = mutableListOf<String>()
+
+        fun buildInputPriorityCalculator(systemInData: String): Int {
+            buildDataFromSystemInData(systemInData)
+            buildNumberListAndArithmeticOperation()
+            return buildSum()
+        }
+
+        private fun buildDataFromSystemInData(systemInData: String) {
+            val stdin = System.`in`
+            System.setIn(ByteArrayInputStream(systemInData.toByteArray()))
+            val scanner = Scanner(System.`in`)
+            System.setIn(stdin)
+            data = scanner.nextLine().split(" ")
+        }
+
+        private fun buildNumberListAndArithmeticOperation() {
+            for (str in data) {
+                try {
+                    numberList.add(str.toInt())
+                } catch (e: NumberFormatException) {
+                    arithmeticOperation.add(str)
+                }
+            }
+        }
+
+        private fun buildSum(): Int {
+            var sum = numberList[START_INDEX]
+            for (i in 1..numberList.lastIndex) {
+                when (arithmeticOperation[i - 1]) {
+                    "+" -> {
+                        sum += numberList[i]
+                    }
+                    "-" -> {
+                        sum -= numberList[i]
+                    }
+                    "/" -> {
+                        sum /= numberList[i]
+                    }
+                    "*" -> {
+                        sum *= numberList[i]
+                    }
+                }
+            }
+            return sum
+        }
+
+        companion object {
+            const val START_INDEX = 0
+        }
+    }
 
     @Test
-    fun `입력 우선 순위 계산기`(){
+    fun inputPriority() {
         //given
-        val data: String = "2 + 3 * 4 / 2"
-        val stdin = System.`in`
-        System.setIn(ByteArrayInputStream(data.toByteArray()))
-        val scanner = Scanner(System.`in`)
-        System.setIn(stdin)
-        println(scanner.nextLine())
-
-        //when
-
-        //then
+        val data = "2 + 10 * 4 / 2"
+        val inputPriority = InputPriorityCalculator()
+        val result = inputPriority.buildInputPriorityCalculator(data)
+        println(result)
     }
 }
