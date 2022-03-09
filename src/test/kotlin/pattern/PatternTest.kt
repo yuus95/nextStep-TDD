0eb3a96f3cb04f31fa39b9a944aef554cc723250
yushin <kkad45@naver.com>
2022-03-09 18:05:57 +0900

    feature(replace) - 문법 테스트 코드 작성

--- src/test/kotlin/pattern/PatternTest.kt
+++ src/test/kotlin/pattern/PatternTest.kt
@@ -12,29 +12,55 @@ class PatternTest {
         if (m.find()) {
             val customDelimiter: String = m.group(1)
             val tokens: List<String> = m.group(2).split(customDelimiter)
+            println(m.group(1))
             println(m.group(2))
             println(tokens)
             // 덧셈 구현
         }
     }
 
-            @Test
-            fun regex() {
-                val str = "//;\n1;2;3"
-                val condition = "//(.)\n(.*)"
-                val regex = Regex(condition)
-                if (regex.containsMatchIn(str)) {
-                    val find: MatchResult? = regex.find(str)
-                    println(find!!.value[0])
+    @Test
+    fun regex() {
+        val str = "//;\n1;2;3"
+        val condition = "//(.)\n(.*)"
+        val regex = Regex(condition)
+        if (regex.containsMatchIn(str)) {
+            val find: MatchResult? = regex.find(str)
+            println(find!!.groupValues[0])
+            println("find!!.groupValues[1] ${find!!.groupValues[1]}" )
+            println("22222 find!!.groupValues[2] ${find!!.groupValues[2]}")
         }
     }
 
     @Test
-    fun numberFiltering(){
+    fun numberFiltering() {
         //given
-        val numberFilter =  Regex("^[1-9]+$")
-        if(numberFilter.containsMatchIn("1234")){
+        val numberFilter = Regex("^[1-9]+$")
+        if (numberFilter.containsMatchIn("1234")) {
             println(true)
+        }
+    }
+
+    @Test
+    fun matchResult() {
+        //given
+        val regex = Regex("//(.)")
+        val input = "//;//a"
+        val find = regex.find(input)
+        val next1 = find?.next()
+        println(find?.value)
+        println(find?.next()?.value ?: "없다")
+        println(next1?.next()?.value ?: "없다")
+    }
+
+    @Test
+    fun match() {
+        val regex = """a([bc]+)d?""".toRegex()
+        val matchResult = regex.find("abcd")
+        val groupValues: List<String> = matchResult!!.groupValues
+        println("-1".toInt())
+        for (value in groupValues) {
+            println(value)
         }
     }
 }
