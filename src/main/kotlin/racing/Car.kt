d2d93f55325af232a23088a69b187af52fdc5e82
yushin <kkad45@naver.com>
2022-03-11 20:21:20 +0900

    feature(racing) - car GetScore 구현

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -2,10 +2,17 @@ package racing
 
 class Car {
     var name: String = ""
+    var number = 0
     fun buildName(inputName: String) {
         if (inputName.length > 5) {
             throw IllegalArgumentException()
         }
         name = inputName
+    }
+
+    fun getScore(randomNumber: Int) {
+        if(randomNumber >= 4){
+             number+=1
+        }
     }
 }
--- src/test/kotlin/racing/RacingTest.kt
+++ src/test/kotlin/racing/RacingTest.kt
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test
 
 class RacingTest {
     private var car = Car()
+    private var racing = Racing()
+    val carNameList = "K5,K3,SM3"
+
 
     @Test
     @DisplayName("각 자동차에 이름부여하기")
@@ -26,11 +29,18 @@ class RacingTest {
     @Test
     @DisplayName("자동차이름 쉼표로 구분하기")
     fun buildCarList(){
-        val carNameList = "K5,K3,SM3"
-        val racing = Racing()
         racing.buildCarList(carNameList)
         Assertions.assertEquals(racing.carList[0].name,"K5")
         Assertions.assertEquals(racing.carList[1].name,"K3")
         Assertions.assertEquals(racing.carList[2].name,"SM3")
     }
+
+    @Test
+    fun carGetScore(){
+        val needNumber = 4
+        car.getScore(4)
+        Assertions.assertEquals(car.number,1)
+    }
+
+
 }
