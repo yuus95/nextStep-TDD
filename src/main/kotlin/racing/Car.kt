93ee4c43358cd0d15ce0125580a331d50680c5ac
yushin <kkad45@naver.com>
2022-03-17 21:53:08 +0900

    feature(racing) - 리팩토링

--- src/main/kotlin/racing/Car.kt
+++ src/main/kotlin/racing/Car.kt
@@ -2,17 +2,29 @@ package racing
 
 class Car {
     var name: String = ""
-    var score = 0
+    var score  = 0
     fun buildName(inputName: String) {
-        if (inputName.length > 5) {
+        if (checkNameLength(inputName)) {
             throw IllegalArgumentException()
         }
         name = inputName
     }
 
     fun getScore(randomNumber: Int) {
-        if(randomNumber >= 4){
-             score+=1
+        if(checkRandomNumberLength(randomNumber)){
+            score+=1
         }
+    }
+
+    private fun checkNameLength(inputName: String):Boolean{
+        return inputName.length > CAR_MAX_LENGTH
+    }
+
+    private fun checkRandomNumberLength(randomNumber: Int):Boolean{
+        return randomNumber >= 4
+    }
+
+    companion object{
+        const val CAR_MAX_LENGTH = 5
     }
 }
